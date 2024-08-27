811fc1e5abf9f137c06cfa2ead97e95271193bea
pradeepxdxd <pradeepbiswas41813@gmail.com>
2024-08-27 19:52:59 +0530

    add : socket.io added for chat

--- app.js
+++ app.js
@@ -2,10 +2,23 @@ import express, { urlencoded } from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
 import cors from 'cors'
+import { Server } from 'socket.io'
+import { createServer } from 'http'
+
 import route from './routes/index.js';
 import dbConnection from './config/db.config.js';
 
 const app = express();
+const server = createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: process.env.APP_URL,
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+        credentials: true
+    }
+})
+
 const PORT = process.env.PORT
 dbConnection()
 app.use(cors())
@@ -14,7 +27,33 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/v1', route);
 
-app.listen(PORT, (_, err) => {
+// socket.io
+const users = {};
+io.on('connection', (socket) => {
+    socket.on('register', userId => {
+        users[userId] = socket.id
+    })
+
+    // Handle sending a message to a specific user
+    socket.on('private message', ({ to, message }) => {
+        const recipientSocketId = users[message.from];
+        if (recipientSocketId) {
+            io.to(recipientSocketId).emit('private message', message);
+        }
+    });
+    
+    socket.on('disconnect', () => {
+        // Remove the user from the list on disconnect
+        for (const [userId, socketId] of Object.entries(users)) {
+            if (socketId === socket.id) {
+                delete users[userId];
+                break;
+            }
+        }
+    });
+})
+
+server.listen(PORT, (_, err) => {
     if (err) console.log('Something went wrong')
     else console.log('Server connected successfully', PORT)
 })
