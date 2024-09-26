e65a36c45223a0c715bac864a101cb12a1b868c4
pradeepxdxd <pradeepbiswas41813@gmail.com>
2024-09-26 11:32:23 +0530

    test : testing google oauth config

--- config/google.config.js
+++ config/google.config.js
@@ -4,10 +4,11 @@ dotenv.config()
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
+const CALLBACK = process.env.APP_URL
 
-console.log({GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET})
+console.log({ GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET })
 export const oauth2client = new google.auth.OAuth2(
     GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET,
-    'http://localhost:3000'
+    CALLBACK
 )
